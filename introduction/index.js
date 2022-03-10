13592e39568b8223580a093b247345a7222238ca
draaaaaaaaaaaaaaa <moh.daffa.ucui@gmail.com>
2022-03-10 13:42:42 +0700

    first - comit

---
+++ introduction/index.js
@@ -0,0 +1,14 @@
+// //import module express
+// const express = require('express');
+// const port = 5000
+// const app = express();
+
+// //create rooting
+// app.get('/',(req,res) => {
+//     res.send('Halo saya belajar express')
+// })
+
+// //mangil
+// app.listen(port,function(){
+//     console.log('server berjalan di server lancar ')
+// })
